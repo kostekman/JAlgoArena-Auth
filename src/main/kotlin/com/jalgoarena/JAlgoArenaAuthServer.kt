946fde654361d9885ffe838999b1b621f79b0cae
Adam Koscinski <kostekman1@gmail.com>
2018-01-16 19:07:24 +0100

    Adding heroku integration, swagger.

--- src/main/kotlin/com/jalgoarena/JAlgoArenaAuthServer.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaAuthServer.kt
@@ -4,13 +4,33 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 @EnableConfigurationProperties
-open class JAlgoArenaAuthServer
+open class JAlgoArenaAuthServer {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(JAlgoArenaAuthServer::class.java, *args)
+        }
+    }
 
-fun main(args: Array<String>) {
-    SpringApplication.run(JAlgoArenaAuthServer::class.java, *args)
+    @Bean
+    open fun authApi(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
 }
+
+
