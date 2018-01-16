3646cfcbaa92783adb3c5a7c9993f5af7036e5eb
Adam Koscinski <kostekman1@gmail.com>
2018-01-16 21:17:37 +0100

    Added security key to swagger.

--- src/main/kotlin/com/jalgoarena/JAlgoArenaAuthServer.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaAuthServer.kt
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -30,6 +31,11 @@ open class JAlgoArenaAuthServer {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(apiKey())
+    }
+
+    private fun apiKey(): ApiKey {
+        return ApiKey("authkey", "Authorization", "header")
     }
 }
 
